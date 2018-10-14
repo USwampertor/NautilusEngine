7be08a27b7f2cc7c8e615a7f0985943028fe97de
Marco Millan <idv16c.mmillan@uartesdigitales.edu.mx>
2018-10-14 15:42:09 -0500

    +Added random and data stream

---
+++ nauEngine/nauUtilities/include/nauDataStream.h
@@ -0,0 +1,21 @@
+/*||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||*/
+/**
+ * @file nauDataStream.h
+ * @author Marco "Swampy" Millan
+ * @date 2018/10/14 2018
+ * @brief
+ * 
+ */
+/*||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||*/
+#pragma once
+namespace nauEngineSDK {
+
+class nauDataStream
+{
+public:
+  nauDataStream() = default;
+  ~nauDataStream() = default;
+};
+}
+
+
